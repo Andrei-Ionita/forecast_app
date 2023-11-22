e7c5c71a843b958e071ec53625486d96b085d6a3
Andrei Ionita <andrei.ionita@mynexte.com>
2023-11-22 16:02:33 +0200

    Introducing the Transavia Forecast Section into the App

--- ml.py
+++ ml.py
@@ -8,6 +8,7 @@ import joblib
 import xlsxwriter
 import os
 import time
+import zipfile
 from datetime import datetime
 
 session_start_time = time.time()
@@ -62,14 +63,360 @@ Public_Holiday = pd.DataFrame({"holiday": "Public Holiday",
 holidays = pd.concat((New_year_and_day_after, National_holiday, Christmas, St_Andrew, Ziua_Principatelor, Adormirea_Maicii_Domnului, Rusalii, Ziua_Copilului, Ziua_Muncii,
 											Pastele, Vinerea_Mare, Ziua_Unirii, Public_Holiday))
 
-file_path = "./Solina/Production/Results_Production_xgb.xlsx"
-def get_file_creation_date(file_path):
-    """Returns the creation date of the file."""
-    if os.path.exists(file_path):
-        creation_time = os.path.getctime(file_path)
-        return datetime.fromtimestamp(creation_time).strftime('%Y-%m-%d')
-    else:
-        return "File not found"
+# ===============================================TRANSAVIA FORECAST=============================================================
+def predicting_exporting_Transavia(dataset):
+	dataset_forecast = dataset
+	CEFs = dataset_forecast.Centrala.unique()
+	datasets_forecast = {elem : pd.DataFrame for elem in CEFs}
+	for CEF in CEFs:
+		print("Predicting for {}".format(CEF))
+		xgb_loaded = joblib.load("./Transavia/Production/Models/rs_xgb_{}.pkl".format(CEF))
+		# dataset_forecast = pd.read_excel("./Transavia/Production/Input_production.xlsx")
+		dataset_forecast = dataset_forecast[:][dataset_forecast.Centrala == CEF]
+		dataset_forecast["Month"] = dataset_forecast.Data.dt.month
+		if CEF in ["F24"]:
+		  df_forecast = dataset_forecast.drop(["Data", "Nori", "Centrala"], axis=1)
+		else:
+		  df_forecast = dataset_forecast.drop(["Data", "Centrala"], axis=1)
+		preds = xgb_loaded.predict(df_forecast.values)
+		# Exporting Results to Excel
+		workbook = xlsxwriter.Workbook("./Transavia/Production/Results/Results_daily_{}.xlsx".format(CEF))
+		worksheet = workbook.add_worksheet("Prediction_Production")
+
+		worksheet.write(0,0,"Data")
+		worksheet.write(0,1,"Interval")
+		worksheet.write(0,2,"Production")
+		date_format = workbook.add_format({'num_format':'dd.mm.yyyy'})
+		row = 1
+		col = 0
+
+		for value in preds:
+		  worksheet.write(row, col+2, value)
+		  row +=1
+		row = row - len(preds)
+		for data in dataset_forecast["Data"]:
+		  worksheet.write(row, col, data, date_format)
+		  row +=1
+		row = row - len(dataset_forecast["Data"])
+		for value in dataset_forecast["Interval"]:
+		  worksheet.write(row, col+1, value)
+		  row +=1
+		workbook.close()
+
+def predicting_exporting_consumption_Santimbru(dataset):
+	# Importing the dataset
+	dataset_forecast = dataset
+	IBDs = dataset_forecast.IBD.unique()
+	IBDs_PVPP = ["Abator", "Abator_Bocsa", "Ciugud", "F5", "Ferma_Bocsa", "FNC", "F4"]
+	datasets_forecast = {elem : pd.DataFrame for elem in IBDs}
+	for IBD in IBDs:
+		datasets_forecast[IBD] = dataset_forecast[:][dataset_forecast.IBD == IBD]
+		datasets_forecast[IBD]["WeekDay"] = datasets_forecast[IBD].Data.dt.weekday
+		datasets_forecast[IBD]["Month"] = datasets_forecast[IBD].Data.dt.month
+		datasets_forecast[IBD]["Holiday"] = 0
+		for holiday in datasets_forecast[IBD]["Data"].unique():
+			if holiday in holidays.ds.values:
+				datasets_forecast[IBD]["Holiday"][datasets_forecast[IBD]["Data"] == holiday] = 1
+		if len(datasets_forecast[IBD]["Flow_Chicks"].value_counts()) > 0 and len(datasets_forecast[IBD]["Radiatie"].value_counts()) > 0:
+		  	## Restructuring the dataset
+			datasets_forecast[IBD] = datasets_forecast[IBD][["Month", "WeekDay","Holiday", "Interval", "Temperatura", "Flow_Chicks", "Radiatie"]]
+		elif len(datasets_forecast[IBD]["Flow_Chicks"].value_counts()) > 0:
+			datasets_forecast[IBD] = datasets_forecast[IBD][["Month", "WeekDay","Holiday", "Interval", "Temperatura", "Flow_Chicks"]]
+		elif len(datasets_forecast[IBD]["Radiatie"].value_counts()) > 0:
+			datasets_forecast[IBD] = datasets_forecast[IBD][["Month", "WeekDay","Holiday", "Interval", "Temperatura", "Radiatie"]]
+		else:
+		  datasets_forecast[IBD] = datasets_forecast[IBD][["Month", "WeekDay","Holiday", "Interval", "Temperatura"]]
+	# datasets_forecast[IBD].replace([np.inf, -np.inf], np.nan)
+	# datasets_forecast[IBD].dropna(inplace = True)
+		# Check if the cons place has PVPP and add the column, if it does
+		if IBD in IBDs_PVPP:
+			datasets_forecast[IBD]["PVPP"] = 1
+	# Predicting
+	predictions = {}
+	for IBD in datasets_forecast.keys():
+		if IBD in IBDs_PVPP:
+		  xgb_loaded = joblib.load("./Transavia/Consumption/Models/rs_xgb_{}.pkl".format(IBD))
+		  print("Predicting for {}".format(IBD))
+		  # print(datasets_forecast[IBD])
+		  xgb_preds = xgb_loaded.predict(datasets_forecast[IBD].drop("PVPP", axis=1).values)
+		  predictions[IBD] = xgb_preds
+		  predictions["Data"] = dataset_forecast["Data"]
+		  predictions["Interval"] = dataset_forecast["Interval"]
+		else:
+			xgb_loaded = joblib.load("./Transavia/Consumption/Models/rs_xgb_{}.pkl".format(IBD))
+			print("Predicting for {}".format(IBD))
+			# print(datasets_forecast[IBD])
+			xgb_preds = xgb_loaded.predict(datasets_forecast[IBD].values)
+			predictions[IBD] = xgb_preds
+			predictions["Data"] = dataset_forecast["Data"]
+			predictions["Interval"] = dataset_forecast["Interval"]
+	# Predicting with PVPP models
+	predictions_PVPP = {}
+	for IBD in datasets_forecast.keys():
+		if IBD in IBDs_PVPP:
+			if os.path.isfile("./Transavia/Consumption/Models/rs_xgb_{}_PVPP.pkl".format(IBD)):
+				xgb_loaded = joblib.load("./Transavia/Consumption/Models/rs_xgb_{}_PVPP.pkl".format(IBD))
+				print("Predicting for {}".format(IBD))
+				# print(datasets_forecast[IBD])
+				xgb_preds = xgb_loaded.predict(datasets_forecast[IBD].drop("Radiatie", axis=1).values)
+				predictions_PVPP[IBD] = xgb_preds
+				predictions_PVPP["Data"] = dataset_forecast["Data"]
+				predictions_PVPP["Interval"] = dataset_forecast["Interval"]
+	# Exporting Results to Excel
+	workbook = xlsxwriter.Workbook("./Transavia/Consumption/Results/XGB/Results_IBDs_daily.xlsx")
+	worksheet = workbook.add_worksheet("Prediction_Consumption")
+
+	worksheet.write(0,0,"Data")
+	worksheet.write(0,1,"Interval")
+	worksheet.write(0,2,"Prediction")
+	worksheet.write(0,3,"IBD")
+	worksheet.write(0,4,"Lookup")
+	worksheet.write(0,5,"PVPP_Model_Prediction")
+	date_format = workbook.add_format({'num_format':'dd.mm.yyyy'})
+	row = 1
+	col = 0
+	for IBD in datasets_forecast.keys():
+		if IBD in predictions_PVPP.keys():
+			for value in predictions[IBD]:
+				worksheet.write(row, col+2, value)
+				worksheet.write(row, col+3, IBD)
+				row +=1
+			row = row-len(predictions[IBD])
+			for value in predictions_PVPP[IBD]:
+				worksheet.write(row, col+5, value)
+				row +=1
+		else:
+			for value in predictions[IBD]:
+				worksheet.write(row, col+2, value)
+				worksheet.write(row, col+3, IBD)
+				row +=1
+	row = row - len(predictions[IBD])*len(datasets_forecast.keys())
+	for data in predictions["Data"]:
+		worksheet.write(row, col, datetime.date(data),date_format)
+		worksheet.write_formula(row, col+4, "=A"+ str(row+1)+ "&" + "B"+ str(row+1)+ "&" + "D" + str(row+1))
+		row +=1
+	row = row - len(predictions["Data"])
+	for interval in predictions["Interval"]:
+		worksheet.write(row, col+1, interval)
+		row +=1
+	# row = 1
+	# for value in y_test:
+	#     worksheet.write(row, col + 1, value)
+	#     row +=1
+	workbook.close()
+
+def predicting_exporting_consumption_Brasov(dataset):
+	# Importing the dataset
+	dataset_forecast = dataset
+	PODs = dataset_forecast.POD.unique()
+	datasets_forecast = {elem : pd.DataFrame for elem in PODs}
+	for POD in PODs:
+		datasets_forecast[POD] = dataset_forecast[:][dataset_forecast.POD == POD]
+		datasets_forecast[POD]["WeekDay"] = datasets_forecast[POD].Data.dt.weekday
+		datasets_forecast[POD]["Month"] = datasets_forecast[POD].Data.dt.month
+		datasets_forecast[POD]["Holiday"] = 0
+		for holiday in datasets_forecast[POD]["Data"].unique():
+		  if holiday in holidays.ds.values:
+			  datasets_forecast[POD]["Holiday"][datasets_forecast[POD]["Data"] == holiday] = 1
+		if len(datasets_forecast[POD]["Flow_Chicks"].value_counts()) > 0 and len(datasets_forecast[POD]["PVPP"].value_counts()) > 0:
+			datasets_forecast[POD] = datasets_forecast[POD][["Month", "WeekDay","Holiday", "Interval", "Temperatura", "Flow_Chicks", "PVPP"]]
+		elif len(datasets_forecast[POD]["Flow_Chicks"].value_counts()) > 0:
+			datasets_forecast[POD] = datasets_forecast[POD][["Month", "WeekDay","Holiday", "Interval", "Temperatura", "Flow_Chicks"]]
+		elif len(datasets_forecast[POD]["PVPP"].value_counts()) > 0:
+			datasets_forecast[POD] = datasets_forecast[POD][["Month", "WeekDay","Holiday", "Interval", "Temperatura", "PVPP"]]
+		else:
+			datasets_forecast[POD] = datasets_forecast[POD][["Month", "WeekDay","Holiday", "Interval", "Temperatura"]]
+		# datasets_forecast[POD].replace([np.inf, -np.inf], np.nan)
+		# datasets_forecast[POD].dropna(inplace = True)
+	# Predicting noPVPP
+	predictions = {}
+	for POD in datasets_forecast.keys():
+		if "PVPP" in datasets_forecast[POD].columns:
+		  xgb_loaded = joblib.load("./Transavia/Consumption/Models/Brasov_Models/rs_xgb_{}.pkl".format(POD))
+		  print("Predicting for {}".format(POD))
+		  # print(datasets_forecast[POD])
+		  xgb_preds = xgb_loaded.predict(datasets_forecast[POD].drop("PVPP", axis=1).values)
+		  predictions[POD] = xgb_preds
+		  predictions["Data"] = dataset_forecast["Data"]
+		  predictions["Interval"] = dataset_forecast["Interval"]
+		else:
+		  xgb_loaded = joblib.load("./Transavia/Consumption/Models/Brasov_Models/rs_xgb_{}.pkl".format(POD))
+		  print("Predicting for {}".format(POD))
+		  # print(datasets_forecast[POD])
+		  xgb_preds = xgb_loaded.predict(datasets_forecast[POD].values)
+		  predictions[POD] = xgb_preds
+		  predictions["Data"] = dataset_forecast["Data"]
+		  predictions["Interval"] = dataset_forecast["Interval"]
+	# Predicting with PVPP
+	predictions_PVPP = {}
+	for POD in datasets_forecast.keys():
+		if os.path.isfile(".Transavia/Consumption/Brasov_Models/rs_xgb_{}_PVPP.pkl".format(POD)):
+		  xgb_loaded = joblib.load("./Transavia/Consumption/Brasov_Models/rs_xgb_{}_PVPP.pkl".format(POD))
+		  print("Predicting for {}".format(POD))
+		  # print(datasets_forecast[POD])
+		  xgb_preds = xgb_loaded.predict(datasets_forecast[POD].values)
+		  predictions_PVPP[POD] = xgb_preds
+		  predictions_PVPP["Data"] = dataset_forecast["Data"]
+		  predictions_PVPP["Interval"] = dataset_forecast["Interval"]
+		# Exporting Results to Excel
+	workbook = xlsxwriter.Workbook("./Transavia/Consumption/Results/XGB/Results_IBDs_daily_Brasov.xlsx")
+	worksheet = workbook.add_worksheet("Prediction_Consumption")
+
+	worksheet.write(0,0,"Data")
+	worksheet.write(0,1,"Interval")
+	worksheet.write(0,2,"Prediction")
+	worksheet.write(0,3,"POD")
+	worksheet.write(0,4,"Lookup")
+	worksheet.write(0,5,"Prediction_PVPP")
+	date_format = workbook.add_format({'num_format':'dd.mm.yyyy'})
+	row = 1
+	col = 0
+	for POD in datasets_forecast.keys():
+		if POD in predictions_PVPP.keys():
+			for value in predictions[POD]:
+				worksheet.write(row, col+2, abs(value))
+				worksheet.write(row, col+3, str(POD))
+				row +=1
+			row = row - len(predictions[POD])
+			for value in predictions_PVPP[POD]:
+				worksheet.write(row, col+5, abs(value))
+				row +=1
+		else:
+			for value in predictions[POD]:
+				worksheet.write(row, col+2, abs(value))
+				worksheet.write(row, col+3, str(POD))
+				row +=1
+	row = row - (len(predictions[POD]) * len(datasets_forecast.keys()))
+	for data in predictions["Data"]:
+		worksheet.write(row, col, datetime.date(data),date_format)
+		worksheet.write_formula(row, col+4, "=A"+ str(row+1) + "&" + "B"+ str(row+1) + "&" +  "D" + str(row+1))
+		row +=1
+		row = row - len(predictions["Data"])
+	for interval in predictions["Interval"]:
+		worksheet.write(row, col+1, interval)
+		row +=1
+	# row = 1
+	# for value in y_test:
+	#     worksheet.write(row, col + 1, value)
+	#     row +=1
+	workbook.close()
+
+def zip_files(folder_path, zip_name):
+	zip_path = os.path.join(folder_path, zip_name)
+	with zipfile.ZipFile(zip_path, 'w') as zipf:
+		for root, _, files in os.walk(folder_path):
+			for file in files:
+				if file != zip_name:  # Avoid zipping the zip file itself
+					file_path = os.path.join(root, file)
+					arcname = os.path.relpath(file_path, folder_path)  # Relative path within the zip file
+					zipf.write(file_path, arcname)
+
+def render_production_forecast_Transavia():
+	st.write("Production Forecast")
+	# ... (other content and functionality for production forecasting)
+	uploaded_files = st.file_uploader("Choose a file", type=["text/csv", "xlsx"], accept_multiple_files=True)
+
+	if uploaded_files is not None:
+		for uploaded_file in uploaded_files:
+			if uploaded_file.type == "text/csv":
+				df = pd.read_csv(uploaded_file)
+			elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+				try:
+					df = pd.read_excel(uploaded_file)
+				except ValueError:
+					st.error("Expected sheet name 'Forecast_Dataset' not found in Excel file.")
+					continue
+			else:
+				st.error("Unsupported file format. Please upload a CSV or XLSX file.")
+				continue
+			st.dataframe(df)
+
+			# Submit button
+			if st.button('Submit'):
+				st.success('Forecast Ready', icon="✅")
+				# Your code to generate the forecast
+				predicting_exporting_Transavia(df)
+				# Creating the ZIP file with the Productions:
+				folder_path = './Transavia/Production/Results'
+				zip_name = 'Transavia_Production_Results.zip'
+				zip_files(folder_path, zip_name)
+				file_path = './Transavia/Production/Results/Transavia_Production_Results.zip'
+
+				with open(file_path, "rb") as f:
+					zip_data = f.read()
+
+				# Create a download link
+				b64 = base64.b64encode(zip_data).decode()
+				button_html = f"""
+					 <a download="Transavia_Production_Results.zip" href="data:application/zip;base64,{b64}" download>
+					 <button kind="secondary" data-testid="baseButton-secondary" class="st-emotion-cache-12tniow ef3psqc12">Download Forecast Results</button>
+					 </a> 
+					 """
+				st.markdown(button_html, unsafe_allow_html=True)
+
+def render_consumption_forecast_Transavia():
+	st.write("Consumption Forecast")
+	# ... (other content and functionality for production forecasting)
+	uploaded_files = st.file_uploader("Choose a file", type=["text/csv", "xlsx"], accept_multiple_files=True)
+
+	if uploaded_files is not None:
+		for uploaded_file in uploaded_files:
+			if uploaded_file.type == "text/csv":
+				df = pd.read_csv(uploaded_file)
+			elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+				try:
+					df = pd.read_excel(uploaded_file)
+				except ValueError:
+					st.error("Expected sheet name 'Forecast_Dataset' not found in Excel file.")
+					continue
+			else:
+				st.error("Unsupported file format. Please upload a CSV or XLSX file.")
+				continue
+			st.dataframe(df)
+
+			# Submit button
+			if st.button('Submit'):
+				st.success('Forecast Ready', icon="✅")
+				# Your code to generate the forecast
+				if uploaded_file.name == "Input.xlsx":
+					predicting_exporting_consumption_Santimbru(df)
+				else:
+					predicting_exporting_consumption_Brasov(df)
+				# Creating the ZIP file with the Productions:
+				folder_path = './Transavia/Consumption/Results/XGB'
+				zip_name = 'Transavia_Consumption_Results.zip'
+				zip_files(folder_path, zip_name)
+				file_path = './Transavia/Consumption/Results/XGB/Transavia_Consumption_Results.zip'
+
+				with open(file_path, "rb") as f:
+					zip_data = f.read()
+
+				# Create a download link
+				b64 = base64.b64encode(zip_data).decode()
+				button_html = f"""
+					 <a download="Transavia_Consumption_Results.zip" href="data:application/zip;base64,{b64}" download>
+					 <button kind="secondary" data-testid="baseButton-secondary" class="st-emotion-cache-12tniow ef3psqc12">Download Forecast Results</button>
+					 </a> 
+					 """
+				st.markdown(button_html, unsafe_allow_html=True)
+
+def render_Transavia_page():
+	
+	# Web App Title
+	st.markdown('''
+	## **The Transavia Forecast Section**
+
+	''')
+
+	# Allow the user to choose between Consumption and Production
+	forecast_type = st.radio("Choose Forecast Type:", options=["Consumption", "Production"])
+
+	if forecast_type == "Consumption":
+		render_consumption_forecast_Transavia()
+	elif forecast_type == "Production":
+		render_production_forecast_Transavia()
+
+#========================================================GENERAL FUNCTIONALITY========================================================
 
 def predicting_exporting(dataset):
 	xgb_loaded = joblib.load("./Solina/Production/rs_xgb_Solina_prod.pkl")
@@ -221,9 +568,11 @@ def render_forecast_page():
 	''')
 
 	# Allow the user to choose between Consumption and Production
-	forecast_type = st.radio("Choose Forecast Type:", options=["Consumption", "Production"])
+	forecast_type = st.radio("Choose Forecast Type:", options=["Consumption", "Production", "Transavia"])
 
 	if forecast_type == "Consumption":
-			render_consumption_forecast()
+		render_consumption_forecast()
 	elif forecast_type == "Production":
-			render_production_forecast()
+		render_production_forecast()
+	elif forecast_type == "Transavia":
+		render_Transavia_page()
