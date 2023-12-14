96694ef8d959a3fb2ac18ad5400717e0e2bedc93
Andrei Ionita <andrei.ionita@mynexte.com>
2023-12-14 16:14:13 +0200

    Small changes to the api key of the Assistant.py

--- assistant.py
+++ assistant.py
@@ -11,7 +11,7 @@ from io import BytesIO
 load_dotenv()
 OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
 client = OpenAI(api_key=OPEN_AI_API_KEY)
-
+print(OPEN_AI_API_KEY)
 
 # --------------------------------------------------------------
 # Upload file
