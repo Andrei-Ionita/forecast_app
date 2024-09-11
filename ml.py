954092a23e698d809a560c747ee763296fcaab83
Andrei Ionita <andrei.ionita@mynexte.com>
2024-09-11 09:09:19 +0300

    Retrieving the database url from secrets

--- ml.py
+++ ml.py
@@ -3240,7 +3240,7 @@ def render_production_forecast():
 				st.markdown(button_html, unsafe_allow_html=True)
 
 def render_forecast_page():
-	st.write(st.secretss)
+	st.print(st.secrets)
 	# Web App Title
 	st.markdown('''
 	# **The Forecast Section**
