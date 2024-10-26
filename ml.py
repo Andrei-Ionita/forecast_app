0d9a9f8531ebbf7467cebb3cb0cb4a80ded1410c
Andrei Ionita <andrei.ionita@mynexte.com>
2024-10-26 17:29:54 +0300

    Switching the Production Forecast to Winter hours

--- ml.py
+++ ml.py
@@ -4158,7 +4158,7 @@ def fetching_Astro_data():
 	columns_to_shift = data.columns.difference(['period_end'])
 
 	# Shift the data columns by 2 intervals
-	data_shifted = data[columns_to_shift].shift(2)
+	data_shifted = data[columns_to_shift].shift(3)
 
 	# Combine the fixed 'period_end' with the shifted data columns
 	data_adjusted = pd.concat([data[['period_end']], data_shifted], axis=1)
